b587f27c6c2a8a7ac0e9693dceb44536b38c1f16
tsai <29006010+tsaaii@users.noreply.github.com>
2018-02-04 07:41:23 -0500

    Add files via upload
    clarity py

---
+++ cl.py
@@ -0,0 +1,11 @@
+# Pip install the client:
+# pip install clarifai
+# gotta install these before
+
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Video as ClVideo
+
+app = ClarifaiApp(api_key='a9fc6d37cd5f429bb1069b89c590a2af')# my api
+model = app.models.get('general-v1.3')
+video = ClVideo(filename='/Users/Owner/Desktop/323.mp4')
+model.predict([video])
